376b178c93fdcc86a5458bcc093c8091a09f78e1
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2025-10-18 16:59:19 +0700

    Update button URLs in start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -32,7 +32,7 @@ def start_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["E_X_1"], url=config.USERBOT),
             InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page")  # About button
         ],
     ]
@@ -59,7 +59,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["E_X_1"],
-                callback_data="fork_repo"
+                callback_data="USERBOT"
             ),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
@@ -85,10 +85,10 @@ def owner_panel(_):
     buttons = [
         [
             InlineKeyboardButton(text=_["S_H_1"], url=config.INSTAGRAM),
-            InlineKeyboardButton(text=_["S_H_2"], url=config.YOUTUBE),
+            InlineKeyboardButton(text=_["S_H_2"], url=config.TIKTOK),
         ],
         [
-            InlineKeyboardButton(text=_["S_H_3"], url=config.GITHUB),
+            InlineKeyboardButton(text=_["S_H_3"], url=config.USERBOT),
             InlineKeyboardButton(text=_["S_H_4"], url=config.DONATE),
         ],
         [
