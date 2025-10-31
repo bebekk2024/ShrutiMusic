8650be5740840121a49d2a20e6a4ddc0785fe6a7
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2025-11-01 02:47:23 +0700

    Update check_channel.py

--- ShrutiMusic/utils/check_channel.py
+++ ShrutiMusic/utils/check_channel.py
@@ -2,14 +2,19 @@ from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
 async def is_joined_channel(client, user_id, support_channel):
     try:
+        print(f"Checking {user_id} in {support_channel}")
         member = await client.get_chat_member(support_channel, user_id)
+        print(f"Status: {member.status}")
         if member.status in ("member", "administrator", "creator"):
             return True
         else:
             return False
     except UserNotParticipant:
+        print("UserNotParticipant")
         return False
     except UserBannedInChannel:
+        print("UserBannedInChannel")
         return False
-    except Exception:
+    except Exception as e:
+        print(f"Other Error: {e}")
         return False
