662246a8bfb3a6e47dfa1ebfde6ccf5a760eba72
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2025-11-05 00:52:01 +0700

    Update voice_welcome.py

--- ShrutiMusic/plugins/tools/voice_welcome.py
+++ ShrutiMusic/plugins/tools/voice_welcome.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import Message
 from ShrutiMusic import app
-import OWNER_ID
+import config
 from ShrutiMusic.misc import db
 from ShrutiMusic.utils.database import music_on, group_assistant, get_lang
 from strings import get_string
